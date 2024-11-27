6387da67fa0a10c4ff514634ee106edec825cc9c
Franklin <71532004+fran1014@users.noreply.github.com>
2024-11-27 18:20:42 +0100

    fix: updated kata.js

--- JavaScript/kata2-hashtag-generator/kata.js
+++ JavaScript/kata2-hashtag-generator/kata.js
@@ -1,23 +1,31 @@
 function generateHashtag(input) {
   // Step 1: Trim the input to remove leading and trailing whitespace. // Example: "   hello world   " -> "hello world"
 
+
   // Step 2: Validate if the input is empty after trimming
+
 
 
   // Step 3: Split the input into words using spaces as a delimiter. // Example: "hello   world" -> ["hello", "world"]
 
 
+
   // Step 4: Capitalize the first letter of each word. // Example: ["hello", "world"] -> ["Hello", "World"]
+
+
 
 
   // Step 5: Join the words into a single string and prepend a hashtag.// Example: ["Hello", "World"] -> "#HelloWorld"
 
 
 
+
   // Step 6: Validate the length of the resulting hashtag. // Example: A hashtag with 141 characters -> false
 
 
+
   // Step 7: Return the valid hashtag. // Example: "#HelloWorld"
+
 
 }
 
